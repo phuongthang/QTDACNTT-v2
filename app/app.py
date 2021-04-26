f10b34c356dae890982309a6318e82ba19cf77f8
Long Nguyen <nhatlong6122@gmail.com>
2021-04-26 21:49:27 +0700

    add facebook login

---
+++ app/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, Blueprint
+from app.database import db
+from app.services.facebook import fb
+
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"db": "devc", "host": "localhost", "port": 27017}
+app.register_blueprint(fb)
+
+db.initialize_db(app)
+model = main_service.init_model()
+
+
+@app.route("/")
+def index():
+    return render_template("authen/login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/show")
+def show():
+    return render_template("campaign/index.html")
+
+@app.route("/create")
+def create():
+    return render_template("campaign/create.html")
+
+
+@app.route("/detail")
+def detail():
+    return render_template("campaign/detail.html")
+
+@app.route("/comments")
+def comments():
+    return render_template("campaign/comment.html")
+
+@app.route("/predicts")
+def predicts():
+    return render_template("campaign/predicts.html")
+
+
+app.run()
---
+++ app/services/facebook.py
@@ -0,0 +1,60 @@
+import os
+
+import flask
+from flask import Blueprint, redirect, url_for
+import requests_oauthlib
+from requests_oauthlib.compliance_fixes import facebook_compliance_fix
+
+# Your ngrok url, obtained after running "ngrok http 5000"
+URL = "https://b2eee988fa35.ngrok.io"
+
+FB_CLIENT_ID = os.environ.get("FB_CLIENT_ID")
+FB_CLIENT_SECRET = os.environ.get("FB_CLIENT_SECRET")
+
+FB_AUTHORIZATION_BASE_URL = "https://www.facebook.com/dialog/oauth"
+FB_TOKEN_URL = "https://graph.facebook.com/oauth/access_token"
+
+FB_SCOPE = ["email"]
+
+# This allows us to use a plain HTTP callback
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+fb = Blueprint("facebook", __name__)
+
+
+@fb.route("/fb-login")
+def login():
+    facebook = requests_oauthlib.OAuth2Session(
+        FB_CLIENT_ID, redirect_uri=URL + "/fb-callback", scope=FB_SCOPE
+    )
+    authorization_url, _ = facebook.authorization_url(FB_AUTHORIZATION_BASE_URL)
+
+    return flask.redirect(authorization_url)
+
+
+@fb.route("/fb-callback")
+def callback():
+    facebook = requests_oauthlib.OAuth2Session(
+        FB_CLIENT_ID, scope=FB_SCOPE, redirect_uri=URL + "/fb-callback"
+    )
+
+    # we need to apply a fix for Facebook here
+    facebook = facebook_compliance_fix(facebook)
+
+    facebook.fetch_token(
+        FB_TOKEN_URL,
+        client_secret=FB_CLIENT_SECRET,
+        authorization_response=flask.request.url,
+    )
+
+    # Fetch a protected resource, i.e. user profile, via Graph API
+
+    facebook_user_data = facebook.get(
+        "https://graph.facebook.com/me?fields=id,name,email,picture{url}"
+    ).json()
+
+    email = facebook_user_data["email"]
+    name = facebook_user_data["name"]
+    picture_url = facebook_user_data.get("picture", {}).get("data", {}).get("url")
+
+    return redirect(url_for('dashboard', name=name, avatar=picture_url))
