abe1a49ab19cb9b663c8d706dac6f91a56850b24
cuongnew87 <82595130+cuongnew87@users.noreply.github.com>
2021-05-31 19:26:53 +0700

    add some comment

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, Blueprint
+from app.api.campaign_api import campaign
+from app.api.comment_api import comment
+from app.api.predict_api import predict
+from app.api.post_api import post
+from app.database import db
+from app.services import main_service
+from app.services.facebook import fb
+
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {"db": "devc", "host": "localhost", "port": 27017}
+app.register_blueprint(predict)
+app.register_blueprint(campaign)
+app.register_blueprint(comment)
+app.register_blueprint(post)
+app.register_blueprint(fb)
+
+db.initialize_db(app)
+model = main_service.init_model()
+
+# Load Login page
+@app.route("/")
+def index():
+    return render_template("authen/login.html")
+
+# Load Dashboard page
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+# Load Index page
+@app.route("/show")
+def show():
+    return render_template("campaign/index.html")
+
+# Load Create comment page
+@app.route("/create")
+def create():
+    return render_template("campaign/create.html")
+
+# Load Detail comment page
+@app.route("/detail")
+def detail():
+    return render_template("campaign/detail.html")
+
+# Load Comment page
+@app.route("/comments")
+def comments():
+    return render_template("campaign/comment.html")
+
+# Load Predict comment page
+@app.route("/predicts")
+def predicts():
+    return render_template("campaign/predicts.html")
+
+# Load Edit comment page
+@app.route("/edit-comment")
+def predicts():
+    return render_template("edit/comment.html")
+    
+app.run()
